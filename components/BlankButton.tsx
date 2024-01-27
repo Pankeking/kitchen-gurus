d34a87796c64a3151a0a25fb7f5cdb7a33e9bf1f
Javier <javiersoap@gmail.com>
2024-01-27 22:43:38 +0100

    blank lines

--- components/BlankButton.tsx
+++ components/BlankButton.tsx
@@ -26,8 +26,6 @@ export default function BlankButton(props: {
             style={{color: themeColors.darkText}}
             />
         </TouchableOpacity>
-        
-          
       </View>
     </>
   )
