a64b7e9a418e3f886df41111ac68bfc9ecc54f51
Javier <javiersoap@gmail.com>
2024-02-08 21:25:26 +0100

    search bar customizing and both sides icons

--- app/(tabs)/search/index.tsx
+++ app/(tabs)/search/index.tsx
@@ -1,13 +1,34 @@
 import { Input } from "@rneui/themed";
-import { Text, View } from "../../../components/themedCustom";
+import { CustomIcon, View } from "../../../components/themedCustom";
 import { StyleSheet } from "react-native";
+import SmallButton from "../../../components/SmallButton";
 
 export default function SearchScreen() {
-  
+
+  const handleSearch = () => {
+    console.log("search")
+    return;
+  }
+
+  const handleCancel = () => {
+    console.log("cancelled")
+    return;
+  }
+
+  const search_icon = <SmallButton  size={40} title="" onPress={handleSearch} iconName="magnify" />
+  const drop_icon = <SmallButton size={40} title="" onPress={handleCancel} iconName={"close"}/>
   return (
       <View style={styles.container}>
-        <Text>Buscar cualqueir wea</Text>
-        <Input placeholder="Search..."></Input>
+        <Input 
+          placeholder="Search" 
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={{borderBottomWidth: 0}}
+          inputStyle={styles.input}
+          leftIcon={search_icon}
+          leftIconContainerStyle={styles.topFix}
+          rightIcon={drop_icon}
+          rightIconContainerStyle={styles.topFix}
+        />
       </View>
   )
 }
@@ -17,5 +38,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
+  inputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 14,
+    width: "90%" 
+  },
+  input: {
+    top: 12,
+    left: 6
+  },
+  topFix: {
+    top: 18
+  },
 })
