9f755f55535dca5bc68ae1c5874f120d4af86f9e
Javier <javiersoap@gmail.com>
2024-02-12 02:59:13 +0100

    renamed shell script automated commit and -p push flag

--- git.sh
+++ git.sh
@@ -1,17 +1,43 @@
 #! /usr/bin/bash
 
+push=false
+message=""
+
+while getopts ":p" opt; do
+  case $opt in
+    p) 
+      push=true
+      message=$2
+      shift
+      ;;
+    \?) 
+      echo "Invalid option: closing"
+      exit 1
+      ;;
+  esac
+done
+if [[ -z $message ]]; then
+  message=$1
+fi
+
 echo "<<<------->>>"
-echo "    Before  "
+echo "    BEFORE  "
 echo "<<<------->>>"
 git status
 git add .
 echo "<<<------->>>"
-echo "    After   "
+echo "    AFTER   "
 echo "<<<------->>>"
 git status
 echo "<<<------->>>"
 echo "     END     "
 echo "<<<------->>>"
-message=$@
+
 git commit -m "$message"
 
+if [[ $push == true ]]; then
+  git push
+  pushMsg="and pushed"
+fi
+
+echo "Commited $pushMsg successfully"
